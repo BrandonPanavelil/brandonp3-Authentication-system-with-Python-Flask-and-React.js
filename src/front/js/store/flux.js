932c29c708d5be7775c84c5b169d50c6f34882e4
Brandon Panavelil <138955239+BrandonPanavelil@users.noreply.github.com>
2024-06-20 01:07:46 +0000

    end of class 6/19/24

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,51 +1,98 @@
+import { Login } from "../pages/login"
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			token: null,
+			signupMessage: null,
+			isSignUpSuccessful: false,
+			loginMessage: null,
+			isLoginSuccessful: false,
+			invoiceMessage: null,
+			invoices: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			//exampleFunction: () => {
+			//	getActions().changeColor(0, "green");
+			//},
+			// getMessage: async () => {
+			// try{
+			//		// fetching data from the backend
+				//	const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+			//		const data = await resp.json()
+			//		setStore({ message: data.message })
+			//		// don't forget to return something, that is how the async resolves
+			//		return data;
+			//	}catch(error){
+			//		console.log("Error loading message from backend", error)
+			//	}
+			//},
+			signUp: async (userEmail, userPassword) => {
+				const options = {
+					method: 'POST',
+					mode: 'cors',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: userEmail,
+						password: userPassword
+					})
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			}
+			
+			const response = await fetch(`${process.env.BACKEND_URL}/api/signup`, options)
+			
+			if (!response.ok) {
+				const data = await response.json()
+				setStore({ signupMessage: data.msg })
+				return {
+					error: {
+						status: response.status,
+						statusText: response.statusText
+					}
+				}
+			}
+			
+			const data = await response.json()
+			setStore({ signupMessage: data.msg })
+				signupMessage: data.msg,
+				isSignUpSuccessful: response.ok
+			})
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			Login: async (userEmail, userPassword) => {
+				const options = {
+					method: 'POST',
+					mode: 'cors',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: userEmail,
+						password: userPassword
+				})
+			}
+			
+			const response = await fetch(`${process.env.BACKEND_URL}/api/signup`, options)
+			
+			if (!response.ok) {
+				const data = await response.json()
+				setStore({ signupMessage: data.msg })
+				return {
+					error: {
+						status: response.status,
+						statusText: response.statusText
+					}
+				}
+			}
+			
+			const data = await response.json()
+			setStore({ signupMessage: data.msg })
+				loginMessage: data.msg,
+				token: data.access_token
+			})	
+			return data;
 			}
 		}
 	};
